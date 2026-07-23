9db22def8ecb4d949e8e67f008d30ac3f54b9155
Rushikesh Donkewar <rushikeshdonkewar23@gmail.com>
2026-07-23 15:22:27 +0530

    Increased JSON body limit to 50mb and added client side image compression

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Static Files (public folder serve karne)
 app.use(express.static('public'));
